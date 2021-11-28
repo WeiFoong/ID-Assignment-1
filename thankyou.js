680ccba7d96a80e22f617eb7e50b5020385729d4
WeiFoong <weifoong2004@gmail.com>
2021-11-29 03:00:24 +0800

    not finished

---
+++ thankyou.js
@@ -0,0 +1,29 @@
+/*function thankYou(){
+    var id = document.getElementById("submit");
+    var text = document.getElementById("msg");
+    if(id.onclick ==true){
+        text.style.display="block";
+    }
+    else{
+        text.style.display="none";
+    }
+}*/
+
+
+/*jQuery(document).ready(function($){
+$(document).on( 'frmFormComplete', function( event, form, response ) {
+var formID = $(form).find('input[name="form_id"]').val();
+});
+});*/
+
+/*let form = document.getElementsByTagName("form")[0];
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Thank you for submitting!");
+});*/
+
+function thankyou() {
+    alert("Thank you for your response!");
+}
+
+
